91e839c3411ed3506968f0a64c637319ecfc75d1
shoxasiroj <shaxobiddinsirojov@gmail.com>
2023-01-29 23:30:42 +0300

    Added 04-variables dir

---
+++ 04-variables/4-dars.py
@@ -0,0 +1,12 @@
+hayot = ('Hello world')
+print(hayot)
+
+
+xabar = ('Bugun xavo yaxshi')
+print(xabar)
+
+
+radius  = 5
+pi = 3.14159
+yuza = pi * radius**2
+print("Radius", radius, "ga teng doira yuzi=", yuza)
