be24f5e1c4dede52e0206428551140b6ba398ce9
shoxasiroj <shaxobiddinsirojov@gmail.com>
2023-01-29 23:42:33 +0300

    6-dars yuklandi

---
+++ 06-numbers/6-dars.py
@@ -0,0 +1,15 @@
+x = int(input('Istalgan son kiritng\n>>>'))
+print(x,' ning kvadrati',x**2,' ga teng')
+print(x,' ning kubi',x**3, 'ga teng')
+
+
+yosh  = int(input('Sizning yoshingiz nechchida?>>>'))
+yosh1 = 2023 - yosh
+print(f"Siz {yosh1}-yilda tug`ilgansiz")
+
+x = int(input('Birinchi sonni kiriting>>>'))
+y = int(input('Ikkinchi sonni kiriting>>>'))
+print(x,'bilan',y, 'ni qo`shilmasi', x+y, 'ga teng')
+print(x,'bilan',y, 'ni ayirmasi',x-y, 'ga teng')
+print(x,'bilan',y,'ni ko`paytmasi',x*y,'ga teng')
+print(x,'bilan',y,'ni bo`linmasi',x/y,'ga teng')
