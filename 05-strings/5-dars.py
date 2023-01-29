d8b588e2f8e98d802dcfd8f4138c1cbb6239d189
shoxasiroj <shaxobiddinsirojov@gmail.com>
2023-01-29 23:36:53 +0300

    5-dars yuklanmoqda

---
+++ 05-strings/5-dars.py
@@ -0,0 +1,9 @@
+kocha  = "bog`bon"
+mahalla  = "\nsag`bon"
+tuman = "\nbodomzor"
+viloyat = "\nsamarqand"
+print(f"{kocha} ko`chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+kocha  = input('Kochangizni nomini kiriting: ')
+mahalla = input('Mahallangizni kiriting: ')
+tuman = input('Tumaningizni nomini kiting: ')
+viloyat = input('Viloyatingizni kiriting: ')
